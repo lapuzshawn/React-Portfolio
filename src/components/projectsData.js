330b1bcf7cd4d58ff130ddadcabf2d4bdf7db318
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-12 19:50:14 -0700

    added Connected App to projects

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,15 +1,21 @@
 const projectsData = [
     {
       title: 'Weather Dashboard',
-      image: 'projectDataImg/M6-Screenshot2.png',
+      image: '../images/ss.png',
       deployedLink: 'https://lapuzshawn.github.io/Weather-Dashboard/',
       githubLink: 'https://github.com/lapuzshawn/Weather-Dashboard',
     },
     {
       title: 'Note Taker',
-      image: 'projectDataImg/M11-Screenshot3.png',
+      image: '../images/ss.png',
       deployedLink: 'https://lapuzshawn.github.io/Note-Taker/',
       githubLink: 'https://github.com/lapuzshawn/Note-Taker',
+    },
+    {
+      title: 'Connected App',
+      image: '../images/ss-connected.png',
+      deployedLink: 'https://connected-g7.herokuapp.com/',
+      githubLink: 'https://github.com/nshaw973/Connected',
     },
     // Add more projects as needed
   ];
